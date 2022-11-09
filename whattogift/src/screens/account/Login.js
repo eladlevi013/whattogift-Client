de64d8d30a9bc865a34765319beabdd3bdade3e4
eladlevi013 <eladlevi013@gmail.com>
2022-11-09 09:36:58 +0200

    updated login

--- whattogift/src/screens/account/Login.js
+++ whattogift/src/screens/account/Login.js
@@ -1,13 +1,75 @@
-import react from "react";
-import {View, Text} from 'react-native';
+import react, {useState} from "react";
+import {View, Text, StyleSheet, TouchableOpacity, Alert} from 'react-native';
+import {TextInput} from 'react-native-paper';
 import Style from '../../utilities/AppStyle';
 
-const Login = () => {
+const Login = (props) => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const login = () => {
+
+    }
+
     return(
-        <View style={Style.container}>
+        <View style={styles.container}>
+          <Text style={styles.title}>{email}</Text>
+    
+          <TextInput
+            label="Email"
+            style={styles.input}
+            keyboardType= 'email-address'
+            autoCapitalize="none"
+            placeholder= "Email"
+            value={email}
+            right={<TextInput.Icon icon="email"/>}
+            onChangeText={text => setEmail(text)}/>
+    
+        <TextInput
+                label="Password"
+                style={styles.input}
+                keyboardType= 'default'
+                placeholder= "Password"
+                value={password}
+                secureTextEntry
+                right={<TextInput.Icon icon="eye"/>}
+                onChangeText={text => setPassword(text)}/>
+    
+          <TouchableOpacity style={styles.btn} onPress={login}>
             <Text>Login</Text>
+          </TouchableOpacity>
+          <Text style={styles.title} onPress={() => {props.navigation.navigate('register')}} >don't have an account?</Text>
         </View>
-    )
+      )
 }
+
+const styles = StyleSheet.create({
+    title: {
+      marginTop: 13,
+      color: "#ffffff"
+    },
+    input: {
+      marginTop: 10,
+      borderRadius: 7,
+      backgroundColor: 'white',
+      width: '80%'
+    },
+    btn: {
+      marginTop: 20,
+      padding: 15,
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: '#F6BE00',
+      width: '80%',
+      borderRadius: 5
+    },
+    container: {
+      backgroundColor: '#3a3b3c',
+      flex: 1,
+      padding: 30,
+      alignItems: 'center',
+      justifyContent: 'center'
+    }
+  })
 
 export default Login;
